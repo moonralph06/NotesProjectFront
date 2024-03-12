3fa5586c1e2de53625f1fcb1fd02ab4a5eac1e8b
Moon <moonralph26@gmail.com>
2024-03-13 04:02:04 +0800

    Implement redux

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/*' element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
